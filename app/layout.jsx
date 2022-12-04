5cd79b3db7cc10a777dcafc6b67f80bf4374e560
Ray Garza <raygarza361@gmail.com>
2022-12-03 18:45:00 -0600

    setup @next/font with google font 'inter'..

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,13 @@
 import './globals.css'
+import { Inter } from '@next/font/google'
+
+const inter = Inter()
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className='inter.className'>{children}</body>
     </html>
   )
 }
