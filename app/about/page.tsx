ce08197df83137a0662f77729d09c88c6090eb65
Ray Garza <raygarza361@gmail.com>
2022-12-12 11:40:43 -0600

    updated styling on home and about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,12 +4,15 @@ import Link from 'next/link'
 export default function About() {
   return (
     <>
-      <div className='p-10 w-screen'>
-        <h1 className='text-5xl font-extrabold text-[#38BDF8] pb-3'>About Page</h1>
-        <p className='pb-10 font-semibold'>Click the button below to go to the Home page.</p>
-
-        <Link href={'/'} className=' font-bold p-4 bg-white bg-opacity-5 hover:bg-opacity-[.07] duraration-200 rounded-lg' >To Home --></Link>
-      </div>
+        <div className='w-[95%] lg:w-5/6 m-auto'>
+          <div className=' py-10'>
+              <h1 className='text-3xl font-extrabold text-[#38BDF8] '>About Page</h1>
+              <p className='pb-10 font-semibold'>Click the button below to go to the Home page.</p>            
+            <div>
+              <Link href={'/'} className='font-bold p-4 bg-white bg-opacity-5 hover:bg-opacity-[.07] duraration-200 rounded-lg border-white border border-opacity-20'>To Home --></Link>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
